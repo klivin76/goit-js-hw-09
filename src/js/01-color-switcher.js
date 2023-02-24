87e5c0198de082c1ac56169269584949a36cb836
Klementiev Ivan <klivin76@gmail.com>
2023-02-24 18:23:31 +0200

    task 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,20 @@
+const startButton = document.querySelector(`button[data-start]`);
+const stopButton = document.querySelector(`button[data-stop]`);
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startButton.addEventListener(`click`, () => {
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+  }, 1000);
+  startButton.setAttribute('disabled', true);
+});
+
+stopButton.addEventListener(`click`, () => {
+  clearInterval(timerId);
+  startButton.removeAttribute('disabled');
+});
